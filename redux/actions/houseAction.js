3137f5a436e6ab52dac6ba67585ce633a8149e52
levi-sr93 <levi-sr@outlook.com>
2020-12-22 20:49:32 -0300

    houseAction fetching data from our API| seeing result data

--- redux/actions/houseAction.js
+++ redux/actions/houseAction.js
@@ -4,6 +4,11 @@ export const CREATE_HOUSES = "CREATE_HOUSES";
 export const fetchHouses = () => {
   return async (dispatch) => {
     //logic to fetch the houses data from the API
+    const result = await fetch("http://localhost:3000/api/houses");
+
+    const resultData = await result.json();
+    console.log(result);
+    console.log(resultData);
 
     dispatch({
       type: FETCH_HOUSES,
